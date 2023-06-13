54ba417e757c927edf0426e5f534c8f3d396cb97
alireza <32122102+ali64iust@users.noreply.github.com>
2023-06-13 08:08:07 +0330

    Update azan.py

--- azan/azan.py
+++ azan/azan.py
@@ -10,8 +10,8 @@ MessageBox = ctypes.windll.user32.MessageBoxW
 #python -m pip install BeautifulSoup4
 #pip install pdfkit
 
-hour_correction=0 #agar jelo bod bayad -1 bezarim
-min_correction=0
+hour_correction=-1 #agar jelo bod bayad -1 bezarim
+min_correction=-15
 
 before_azan=15
 
@@ -25,6 +25,7 @@ def getAzanZohrData():
     azan_zohr_text=azan_zohr.text
     azan_zohr_h=int(azan_zohr_text.split(":")[0])
     azan_zohr_m=int(azan_zohr_text.split(":")[1])
+    print("azan johr: ",azan_zohr_h,":",azan_zohr_m)
     return azan_zohr_h,azan_zohr_m
 
 def get_h_m():
@@ -40,19 +41,22 @@ def check_Azan(h,m,azan_zohr_h1, azan_zohr_m1):
         MessageBox(None, 'azan', 'azan', 0)
 
 d=1
+azan_zohr_h1=0
+azan_zohr_m1=0
+h=0
+m=0
+
 while(1==1):
-    if (d != datetime.now().day):
-        d=datetime.now().day
-        azan_zohr_h1, azan_zohr_m1 = getAzanZohrData()
+    try:
+        if (d != datetime.now().day):
+            azan_zohr_h1, azan_zohr_m1 = getAzanZohrData()
+            d = datetime.now().day
 
-    for i in range(1,61):
-        h,m = get_h_m()
-        check_beforeAzan(h,m,azan_zohr_h1, azan_zohr_m1)
-        check_Azan(h, m, azan_zohr_h1, azan_zohr_m1)
-        time.sleep(30)
-
-
-
-
-
-
+        for i in range(1,61):
+            h,m = get_h_m()
+            check_beforeAzan(h,m,azan_zohr_h1, azan_zohr_m1)
+            check_Azan(h, m, azan_zohr_h1, azan_zohr_m1)
+            time.sleep(30)
+    except Exception as e:
+        print(e)
+        #print()
